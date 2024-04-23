c0122561990bab8581213025cf466ee9ebee99b3
Majid Ali <lahmajidali@gmail.com>
2024-04-23 22:27:42 +0500

    created replaceTemplate as custome module

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const http = require("http");
-const { CLIENT_RENEG_LIMIT } = require("tls");
 const url = require("url");
+
+const replaceTemplate = require('./modules/replaceTemplate');
 
 // FILES
 
@@ -44,20 +45,6 @@ const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   "utf-8"
 );
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%TITLE%}/g, product.productName);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
   const { pathname, query } = url.parse(req.url, true);
